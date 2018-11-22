e7b83795ccedb44358ed9622ed4a98b07aab75a3
Tomas <tomasmichael995@gmail.com>
2018-11-22 19:33:37 +0100

    Wifi p2p and receiver compontents initializations in device list activity

--- app/src/main/java/gr/kalymnos/skemelio/p2pchat/mvc_controllers/DeviceListActivity.java
+++ app/src/main/java/gr/kalymnos/skemelio/p2pchat/mvc_controllers/DeviceListActivity.java
@@ -6,18 +6,21 @@ import android.net.wifi.p2p.WifiP2pManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.widget.Toast;
 
+import gr.kalymnos.skemelio.p2pchat.mvc_model.WiFiDirectBroadcastReceiver;
 import gr.kalymnos.skemelio.p2pchat.mvc_views.DeviceListViewMvc;
 import gr.kalymnos.skemelio.p2pchat.mvc_views.DeviceListViewMvcImp;
 
 import static android.net.wifi.p2p.WifiP2pManager.Channel;
 
-public class DeviceListActivity extends AppCompatActivity implements DeviceListViewMvc.OnDeviceClickListener {
+public class DeviceListActivity extends AppCompatActivity implements DeviceListViewMvc.OnDeviceClickListener, WifiP2pManager.ChannelListener {
 
     private DeviceListViewMvc viewMvc;
 
     private WifiP2pManager manager;
     private Channel channel;
+    private boolean retryChannel = false;
     private boolean isWifiP2pEnabled = false;
 
     private BroadcastReceiver receiver;
@@ -26,22 +29,19 @@ public class DeviceListActivity extends AppCompatActivity implements DeviceListV
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        initializeViewMvc();
-        setSupportActionBar(viewMvc.getToolbar());
-        setContentView(viewMvc.getRootView());
+        setupUi();
+        initializeWifiReceiver();
+        addActionsToIntentFilter();
     }
 
-    private void addActionsToFilter() {
-        filter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
-        filter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
-        filter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
-        filter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
-
-    }
 
     @Override
     public void onDeviceClicked(int position) {
         // TODO: Must implement
+    }
+
+    public void setIsWifiP2pEnabled(boolean isWifiP2pEnabled) {
+        this.isWifiP2pEnabled = isWifiP2pEnabled;
     }
 
     private void initializeViewMvc() {
@@ -49,7 +49,36 @@ public class DeviceListActivity extends AppCompatActivity implements DeviceListV
         viewMvc.setOnDeviceClickListener(this);
     }
 
-    public void setIsWifiP2pEnabled(boolean isWifiP2pEnabled) {
-        this.isWifiP2pEnabled = isWifiP2pEnabled;
+    private void setupUi() {
+        initializeViewMvc();
+        setSupportActionBar(viewMvc.getToolbar());
+        setContentView(viewMvc.getRootView());
+    }
+
+    private void initializeWifiReceiver() {
+        manager = (WifiP2pManager) getSystemService(WIFI_P2P_SERVICE);
+        channel = manager.initialize(this, getMainLooper(), this);
+        receiver = new WiFiDirectBroadcastReceiver(manager, channel, this);
+    }
+
+    private void addActionsToIntentFilter() {
+        filter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        filter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        filter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        filter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+    }
+
+    @Override
+    public void onChannelDisconnected() {
+        // we will try once more
+        if (manager != null && !retryChannel) {
+            Toast.makeText(this, "Channel lost. Trying again", Toast.LENGTH_LONG).show();
+            retryChannel = true;
+            manager.initialize(this, getMainLooper(), this);
+        } else {
+            Toast.makeText(this,
+                    "Severe! Channel is probably lost premanently. Try Disable/Re-Enable P2P.",
+                    Toast.LENGTH_LONG).show();
+        }
     }
 }
