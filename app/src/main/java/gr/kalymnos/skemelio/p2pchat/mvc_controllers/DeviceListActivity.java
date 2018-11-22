0f1bf5b3bafa4ccbea0ac59bbc00604ff17753eb
Tomas <tomasmichael995@gmail.com>
2018-11-22 17:40:46 +0100

    Initial commit

---
+++ app/src/main/java/gr/kalymnos/skemelio/p2pchat/mvc_controllers/DeviceListActivity.java
@@ -0,0 +1,12 @@
+package gr.kalymnos.skemelio.p2pchat.mvc_controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DeviceListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
