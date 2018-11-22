31cbb0b5f5807d958a348785fff7c58e67bf8ea9
Tomas <tomasmichael995@gmail.com>
2018-11-22 20:56:52 +0100

    Created ConnectionErrorText.java

---
+++ app/src/main/java/gr/kalymnos/skemelio/p2pchat/mvc_model/ConnectionErrorText.java
@@ -0,0 +1,18 @@
+package gr.kalymnos.skemelio.p2pchat.mvc_model;
+
+import android.net.wifi.p2p.WifiP2pManager;
+
+public interface ConnectionErrorText {
+    static String getReasonText(int reason){
+        switch (reason){
+            case WifiP2pManager.P2P_UNSUPPORTED:
+                return "Operation failed because p2p is unsupported on the device.";
+            case WifiP2pManager.BUSY:
+                return "Operation failed because the framework is busy and unable to service the request.";
+            case WifiP2pManager.ERROR:
+                return "Operation failed due to an internal error. ";
+                default:
+                    return "Unknown reason";
+        }
+    }
+}
