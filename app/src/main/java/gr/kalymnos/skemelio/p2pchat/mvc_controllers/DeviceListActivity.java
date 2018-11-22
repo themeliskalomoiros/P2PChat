ef83df509c459f5a7198ae3072d29035ecad060e
Tomas <tomasmichael995@gmail.com>
2018-11-22 19:00:04 +0100

    DeviceListActivity implements OnDeviceClickListener and initializes its layout

--- app/src/main/java/gr/kalymnos/skemelio/p2pchat/mvc_controllers/DeviceListActivity.java
+++ app/src/main/java/gr/kalymnos/skemelio/p2pchat/mvc_controllers/DeviceListActivity.java
@@ -2,11 +2,29 @@ package gr.kalymnos.skemelio.p2pchat.mvc_controllers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 
-public class DeviceListActivity extends AppCompatActivity {
+import gr.kalymnos.skemelio.p2pchat.mvc_views.DeviceListViewMvc;
+import gr.kalymnos.skemelio.p2pchat.mvc_views.DeviceListViewMvcImp;
+
+public class DeviceListActivity extends AppCompatActivity implements DeviceListViewMvc.OnDeviceClickListener {
+
+    private DeviceListViewMvc viewMvc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initializeViewMvc();
+        setContentView(viewMvc.getRootView());
+    }
+
+    @Override
+    public void onDeviceClicked(int position) {
+        // TODO: Must implement
+    }
+
+    private void initializeViewMvc() {
+        viewMvc = new DeviceListViewMvcImp(LayoutInflater.from(this), null);
+        viewMvc.setOnDeviceClickListener(this);
     }
 }
