7046ae11ab764180de3045d2abed0598028f32a4
Tomas <tomasmichael995@gmail.com>
2018-11-22 19:19:03 +0100

    DeviceListActivity created wifi and receiver specific fields

--- app/src/main/java/gr/kalymnos/skemelio/p2pchat/mvc_controllers/DeviceListActivity.java
+++ app/src/main/java/gr/kalymnos/skemelio/p2pchat/mvc_controllers/DeviceListActivity.java
@@ -1,5 +1,8 @@
 package gr.kalymnos.skemelio.p2pchat.mvc_controllers;
 
+import android.content.BroadcastReceiver;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -7,16 +10,24 @@ import android.view.LayoutInflater;
 import gr.kalymnos.skemelio.p2pchat.mvc_views.DeviceListViewMvc;
 import gr.kalymnos.skemelio.p2pchat.mvc_views.DeviceListViewMvcImp;
 
+import static android.net.wifi.p2p.WifiP2pManager.Channel;
+
 public class DeviceListActivity extends AppCompatActivity implements DeviceListViewMvc.OnDeviceClickListener {
 
     private DeviceListViewMvc viewMvc;
 
+    private WifiP2pManager manager;
+    private Channel channel;
     private boolean isWifiP2pEnabled = false;
 
+    private BroadcastReceiver receiver;
+    private final IntentFilter filter = new IntentFilter();
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         initializeViewMvc();
+        setSupportActionBar(viewMvc.getToolbar());
         setContentView(viewMvc.getRootView());
     }
 
