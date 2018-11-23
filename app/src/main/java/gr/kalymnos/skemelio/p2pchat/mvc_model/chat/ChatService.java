66618b40c96bbe62f1c453ca06d38204a3e6e27d
Tomas <tomasmichael995@gmail.com>
2018-11-23 15:57:21 +0100

    Created startServer() and startClient for ChatService

--- app/src/main/java/gr/kalymnos/skemelio/p2pchat/mvc_model/chat/ChatService.java
+++ app/src/main/java/gr/kalymnos/skemelio/p2pchat/mvc_model/chat/ChatService.java
@@ -8,16 +8,18 @@ import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.Socket;
 
 import gr.kalymnos.skemelio.p2pchat.pojos.Message;
 
+// TODO: Must implement stopServer and stopClient some time.
+
 public class ChatService implements Server.OnServerAcceptConnectionListener,
         Client.OnClientConnectionListener, MessageReader.OnMessageReceivedListener {
-
     private static final String TAG = "ChatService";
-    private static ChatService instance = null;
 
+    private static ChatService instance = null;
 
     protected Context context;
     private Server server;
@@ -28,7 +30,7 @@ public class ChatService implements Server.OnServerAcceptConnectionListener,
         this.context = context;
     }
 
-    public synchronized ChatService getInstance(@NonNull Context context) {
+    public static synchronized ChatService getInstance(@NonNull Context context) {
         if (instance == null) {
             instance = new ChatService(context);
         }
@@ -65,10 +67,27 @@ public class ChatService implements Server.OnServerAcceptConnectionListener,
         messageReader.start();
     }
 
+    public void startServer() {
+        if (server == null) {
+            server = new Server();
+            server.addOnServerAcceptConnectionListener(this);
+            server.start();
+        }
+    }
+
+    public void startClient(InetAddress serverAddress) {
+        if (client == null) {
+            client = new Client(serverAddress);
+            client.addOnClientConnectionListener(this);
+            client.start();
+        }
+    }
+
     private void initializeMessageReader(Socket socket) {
         if (messageReader == null) {
             try {
                 messageReader = new MessageReader(socket.getInputStream());
+                messageReader.addOnMessageReceivedListener(this);
             } catch (IOException e) {
                 Log.e(TAG, "Error creating MessageReager", e);
             }
